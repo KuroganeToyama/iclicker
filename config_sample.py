95d2f07d8013f910d2215f24f35c27fdd4befc2f
Di Nguyen <vdnguyen@uwaterloo.ca>
2025-01-15 16:46:07 -0500

    Made note clearer

--- config_sample.py
+++ config_sample.py
@@ -1,4 +1,4 @@
 EMAIL = 'insert your email address'
 PASSWORD = 'insert your password'
-DOWNLOAD_PATH = 'insert the full path to your Downloads folder (or whichever folder stores the downloaded files from iClicker Cloud)'
+DOWNLOAD_PATH = 'insert the full path to your Downloads folder (or whichever folder that stores the downloaded files when you manually download them from iClicker Cloud, since iClicker Cloud does not let you choose the download path and only uses the browser default)'
 EXCEPTIONS = ['a list of polls to ignore. for example: EXCEPTIONS = ["Class 1 - Poll", "Class 2 - Poll"]']
