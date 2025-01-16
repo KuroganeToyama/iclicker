7eea9daec5817d4348be97fced9ab6a725032a1c
KuroganeToyama <dingyen96@gmail.com>
2025-01-15 19:21:40 -0500

    Added documentation

--- config_sample.py
+++ config_sample.py
@@ -7,9 +7,11 @@ PASSWORD = ''
 # The full path to your Downloads folder, or whichever folder that stores the downloaded files 
 # when you manually download them from iClicker Cloud, since iClicker Cloud does not let you choose 
 # the download path and only uses the browser default
+# Examples: DOWNLOAD_PATH = '/Users/me/Downloads' (MacOS/Linux would look something like this)
+#           DOWNLOAD_PATH = 'C:/Users/me/Downloads' (Windows would look something like this)
 DOWNLOAD_PATH = ''
 
 # List of polls (i.e. classes) to ignore during data collection
-# Example: EXCEPTIONS = ["Class 1 - Poll", "Class 2 - Poll"]
+# Examples: EXCEPTIONS = ["Class 1 - Poll", "Class 2 - Poll"]
 # Note: I have plans to update the script to accomodate exceptions in multiple sections, but not now
 EXCEPTIONS = []
